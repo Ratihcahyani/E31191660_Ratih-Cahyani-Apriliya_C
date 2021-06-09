7358d76e71b3d4a64793b3fe42c9a4bdc359e09d
Ratihcahyani <66702458+Ratihcahyani@users.noreply.github.com>
2021-06-09 17:06:27 +0700

    Api Volley

---
+++ APIVolley/APIVolley/app/src/main/java/com/example/apivolley/Util/ServerAPI.java
@@ -0,0 +1,11 @@
+package com.example.apivolley.Util;
+/**
+ * Setting alamat API CRUD yang telah dibuat pada CodeIgniter
+ */
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.1.4/ci/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.1.4/ci/mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.1.4/ci/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.1.4/ci/Mahasiswa/ApiUpdate";
+}
+
